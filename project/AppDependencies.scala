dae5e7a2d841f3730cc78770591840d82d5a18dd
ShivaniJain-21 <180167518+ShivaniJain-21@users.noreply.github.com>
2024-10-03 07:52:43 +0100

    DLS-10134 Removed commented old  versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,6 @@ import sbt._
 object AppDependencies {
 
   private val playVersion = "play-30"
-  //private val bootstrapVersion = "9.4.0"
   private val bootstrapVersion = "9.5.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -14,7 +13,6 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"   %% s"bootstrap-test-$playVersion" % bootstrapVersion,
     "org.scalamock" %% "scalamock"                    % "5.2.0"
-    //"org.scalamock" %% "scalamock"                    % "6.0.0"
   ).map(_ % "test, it")
 
   val all: Seq[ModuleID] = compile ++ test
